d1e31ab21ba22ec9b0c87a7a9b1d4bfbd227ba5d
dongglee <leedong1848@gmail.com>
2023-01-16 21:06:20 +0900

    split function

--- srcs/file_reader/setting_map.c
+++ srcs/file_reader/setting_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   setting_map.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yooh <yooh@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: dongglee <dongglee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 22:58:29 by dongglee          #+#    #+#             */
-/*   Updated: 2023/01/16 18:52:00 by yooh             ###   ########.fr       */
+/*   Updated: 2023/01/16 21:05:24 by dongglee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,13 +81,10 @@ int	set_map_emt(t_global *global, t_list *lst)
 	return (!flag);
 }
 
-int	set_map(t_global *global, t_list *lst)
+t_list	*skip_empty_line(t_list *cur)
 {
-	t_list	*cur;
 	char	*temp;
-	int		i;
 
-	cur = lst;
 	temp = (char *)cur->content;
 	while (cur && temp[0] == '\n')
 	{
@@ -96,6 +93,15 @@ int	set_map(t_global *global, t_list *lst)
 			break ;
 		temp = (char *)cur->content;
 	}
+	return (cur);
+}
+
+int	set_map(t_global *global, t_list *lst)
+{
+	t_list	*cur;
+	int		i;
+
+	cur = skip_empty_line(lst);
 	if (cur == NULL)
 		return (1);
 	set_map_size(global, cur);
