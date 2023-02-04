331837b5ef981c0b29aad383d7a8704105664ce6
Taeyoung Yang <tayang@c3r5s3.42seoul.kr>
2023-02-04 21:53:38 +0900

    [DOCS] 주석 추가

--- srcs/Server/ServerService.hpp
+++ srcs/Server/ServerService.hpp
@@ -5,6 +5,9 @@
 #include "Channel/ChannelManager.hpp"
 #include "User/UserManager.hpp"
 
+/*
+** @brief 가능하면 모든 기능은 해당 클래스를 통해서 사용하는 것을 권장함
+*/
 class ServerService {
 private:
     ChannelManager  _channelManager;
@@ -18,14 +21,68 @@ public:
     ServerService(void);
     ~ServerService(void);
 
+    /*
+    * @param user_name: 찾고 싶은 유저 이름
+    * @return User 객체의 포인터 값 (delete하면 안됨)
+    * @throw 찾지 못하면 ServerService::UserNotExist를 던짐
+    */
     User* getUserWithName(const string& user_name) const;
+    
+    /*
+    * @param fd: 찾고 싶은 유저의 fd
+    * @return User 객체의 포인터 값 (delete하면 안됨)
+    * @throw 찾지 못하면 ServerService::UserNotExist를 던짐
+    */
     User* getUserWithFD(const int& fd) const;
+
+    /*
+    * @param channel_name: 찾고 싶은 채널의 이름
+    * @return Channel 객체의 포인터 값 (delete하면 안됨)
+    * @throw 찾지 못하면 ServerService::ChannelNotExist를 던짐
+    */
     Channel* getChannelWithName(const string& channel_name) const;
+
+    /*
+    * @param user_name: 추가할 유저의 이름
+    * @param fd: 추가할 유저의 fd
+    * @brief 만약 유저가 있다면, 아무것도 하지 않음
+    */
     void addUser(const string& user_name, const int& fd);
+
+    /*
+    * @param user_name: 지우고 싶은 유저의 이름
+    * @throw 만약 없는 유저를 지운다면,ServerService::UserNotExist를 던짐
+    */
     void deleteUserWithName(const string& user_name);
+
+    /*
+    * @param channel_name: 들어갈 채널 이름
+    * @param user_name: 들어갈 유저 이름
+    * @throw 만약 없는 유저라면, ServerService::UserNotExist를 던짐
+    * @brief 만약 채널이 존재하지 않는다면 새로운 채널을 만듬
+    */
     void joinChannelWithUserName(const string& channel_name, const string& user_name);
+
+    /*
+    * @param channel_name: 나갈 채널 이름
+    * @param user_name: 나갈 유저 이름
+    * @throw 만약 없는 채널이라면 ServerService::ChannelNotExist를 던짐
+    * @throw 만약 없는 유저라면 ServerService::UserNotExist를 던짐
+    */
     void partChannelWithUserName(const string& channel_name, const string& user_name);
+
+    /*
+    * @param channel_name: 검색할 채널 이름
+    * @throw 만약 없는 채널이라면 ServerService::ChannelNotExist를 던짐
+    * @return 해당 채널에 들어있는 User객체의 포인터값들의 vector를 리턴함
+    */
     vector<User*> getUsersInChannel(const string& channel_name) const;
+
+    /*
+    * @param user_name: 검색할 유저의 이름
+    * @throw 만약 없는 유저라면 ServerService::UserNotExist를 던짐
+    * @return 해당 유저가 들어가 있는 Channel객체의 포인터값들의 vector를 리턴함
+    */
     vector<Channel*> getChannelsFromUser(const string& user_name) const;
       
 
