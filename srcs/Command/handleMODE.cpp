bf39a86723df2d126d402718aed92890375c3b0d
yubinquitous <yb9854@naver.com>
2023-02-06 15:27:38 +0900

    [CHORE] command mode 진행중

--- srcs/Command/handleMODE.cpp
+++ srcs/Command/handleMODE.cpp
@@ -1,5 +1,6 @@
 #include "Command.hpp"
 #include "Utils.hpp"
+#include "ReplieFactory.hpp"
 
 void Command::_handleMODE(Server &server, int fd, const string &msg) {
     // user, channel 구분: 시작 '#'
@@ -23,11 +24,7 @@ void Command::_handleMODE(Server &server, int fd, const string &msg) {
         if (targetNickname != nickname) {
             // :irc.local 502 nickname_ :Can't change mode for other users / :Can't view modes for other users
 			string res = nickname;
-			if (words.size() == 2) {
-				res += " :Can't view modes for other users\n";
-			} else {
-				res += " :Can't change mode for other users\n";
-			}
+			res += ERR_USERSDONTMATCH_502();
 			sendMessage(fd, res);
             return ;
         }
@@ -58,42 +55,47 @@ void Command::_handleMODE(Server &server, int fd, const string &msg) {
             if (newMode[i] == 'i') {
 				// cout << "userMode: " << user->isSetMode((UserMode)USER_MODE_I) << endl;	// TEST
                 if (i > 0 && newMode[i-1] == '-' && user->isSetMode(USER_I)) {
-                    // TODO: user->setInvisible(false);
+                    user->unsetMode(USER_I);
 					// cout << "remove invisible" << endl; // TEST
 					removeMode += "i";
                 } else if (user->isSetMode(USER_I) == 0) {                    
-                    // TODO: user->setInvisible(true);
+                    user->setMode(USER_I);
 					// cout << "add invisible" << endl;	// TEST
 					addMode += "i";
                 }
             } else if (newMode[i] == 'w') {
-                if (i && newMode[i-1] == '-' && user->isSetMode((UserMode)USER_MODE_W)) {
+                if (i && newMode[i-1] == '-' && user->isSetMode(USER_W)) {
                     // TODO: user->setReceiveWallops(false);
+					user->unsetMode(USER_W);
 					removeMode += "w";
-                } else if (user->isSetMode((UserMode)USER_MODE_W) == 0) {
+                } else if (user->isSetMode(USER_W) == 0) {
                     // TODO: user.setReceiveWallops(true);
+					user->setMode(USER_W);
 					addMode += "w";
                 }
             } else if (newMode[i] == 'o') {
-                if (i && newMode[i-1] == '-' && user->isSetMode((UserMode)USER_MODE_O)) {
+                if (i && newMode[i-1] == '-' && user->isSetMode(USER_O)) {
                     // TODO: user->setOperator(false);
+					user->unsetMode(USER_O);
 					removeMode += "o";
-                } else if (user->isSetMode((UserMode)USER_MODE_O) == 0) {
+                } else if (user->isSetMode(USER_O) == 0) {
                     // TODO: user->setOperator(true);
+					user->setMode(USER_O);
 					addMode += "o";
                 }
             } else if (newMode[i] == 's') {
-                if (i && newMode[i-1] == '-' && user->isSetMode((UserMode)USER_MODE_S)) {
-					// TODO: serviceNotice
+                if (i && newMode[i-1] == '-' && user->isSetMode(USER_S)) {
+					user->unsetMode(USER_S);
 					removeMode += "s";
-                } else if (user->isSetMode((UserMode)USER_MODE_S) == 0) {
-					// TODO: serviceNotice
+                } else if (user->isSetMode(USER_S) == 0) {
+					user->setMode(USER_S);
 					addMode += "s";
                 }
             } else if (newMode[i] == '+' || newMode[i] == '-') {
 				continue;
 			} else { // yubchoi j :is unknown mode char to me
-				res += nickname + " " + newMode[i] + " :is unknown mode char to me\n";
+				// replieFactory
+				res += ERR_UNKNOWNMODE_472(newMode[i]);
 			}
         }
         
@@ -104,7 +106,7 @@ void Command::_handleMODE(Server &server, int fd, const string &msg) {
 			res += "MODE " + nickname + " " + removeMode + "\n";
 		}
 		sendMessage(fd, res);
-		cout << "OUTPUT: " << res; // TEST
+		// cout << "OUTPUT: " << res; // TEST
 		return;
     } else {
         // channel MODE: _handleChannelMode()
