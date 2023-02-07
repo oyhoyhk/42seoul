2e93ad2db585f34ec09fe19ebbef47a8e655666a
donggyu <leedong1848@gmail.com>
2023-02-07 16:39:03 +0900

    [FEATURE] Add operator on server

--- srcs/Server/ServerService.cpp
+++ srcs/Server/ServerService.cpp
@@ -46,9 +46,10 @@ void ServerService::deleteUserWithFD(const int& fd) {
 
 Channel* ServerService::joinChannelWithUserName(const string& channel_name, const string& user_name) {
 	Channel* channel = NULL;
+	User* user = getUserWithName(user_name);
 	channel = _channelManager.addChannel(channel_name);
 	if (channel == NULL) channel = _channelManager.getChannelWithName(channel_name);
-	User* user = getUserWithName(user_name);
+	else channel->setOperator(user);
 	user->joinChannel(channel);
 	channel->addUser(user);
 	return channel;
